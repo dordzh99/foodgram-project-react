7a959b5d6488832d52201022f3e0225d91182122
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-10-16 19:31:12 +0300

    review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,7 +4,8 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsAuthorOrAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
+        return (request.method in SAFE_METHODS
+                or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
