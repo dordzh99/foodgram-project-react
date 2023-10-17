f055d338d60abef49943b9b47c32f2e474563ffd
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-10-17 06:08:01 +0300

    review2

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
