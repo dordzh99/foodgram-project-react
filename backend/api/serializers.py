b36187c5dd8ea69d313daba638cb1abe80e5606a
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-10-17 21:10:30 +0300

    review3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
-from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
