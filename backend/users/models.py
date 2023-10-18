16320fe5e15481bb3ee88eb169e91c52eed71731
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-10-18 18:01:11 +0300

    review3

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .constant import LENGTH_EMAIL
-
 
 class CustomUser(AbstractUser):
     """Кастомная модель пользователя."""
