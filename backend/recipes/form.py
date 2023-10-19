50da27a4cca468fe41ce40ff3d2a10b03da42467
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-10-19 04:59:42 +0300

    review3

--- backend/recipes/form.py
+++ backend/recipes/form.py
@@ -7,7 +7,7 @@ class AtLeastOneRequiredInlineFormSet(BaseInlineFormSet):
     def clean(self):
         super().clean()
         if any(self.errors):
-        return
+            return
         if not any(cleaned_data and not cleaned_data.get('DELETE', False)
                    for cleaned_data in self.cleaned_data):
             raise ValidationError('Хотя бы один элемент обязателен.')
